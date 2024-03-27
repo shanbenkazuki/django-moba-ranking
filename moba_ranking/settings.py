d1fb840b195d257bd21582bc74a9c2b78d6a72c0
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-03-28 02:23:16 +0900

    TIME_ZONEとLANGUAGE_CODEを変更

--- moba_ranking/settings.py
+++ moba_ranking/settings.py
@@ -91,9 +91,12 @@ WSGI_APPLICATION = 'moba_ranking.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
@@ -120,9 +123,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
