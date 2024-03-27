b4002676ef41ce6be409932a9d2e0963557414f8
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-03-28 02:54:09 +0900

    heroのモデルを作成

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Hero(models.Model):
+    ROLE_CHOICES = [
+        ('Support', 'Support'),
+        ('Fighter', 'Fighter'),
+        ('Tank', 'Tank'),
+        ('Assassin', 'Assassin'),
+        ('Mage', 'Mage'),
+        ('Marksman', 'Marksman'),
+    ]
+
+    name_jp = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    image_url = models.URLField()
+    article_url = models.URLField()
+
+    def __str__(self):
+        return self.name_en
