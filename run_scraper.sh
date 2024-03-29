e97c1589c5433259125a7c7d318b1a687f06ccb5
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-03-29 11:10:09 +0900

    3日ごとに実行されるように修正

---
+++ run_scraper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /Users/yamamotokazuki/develop/django-moba-ranking/moba-ranking-env/bin/activate
+python /Users/yamamotokazuki/develop/django-moba-ranking/moba_ranking/manage.py run_scraper
