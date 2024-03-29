6839816f2e0fb593d36a1dec00a642f0121fe4ba
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-03-30 00:31:54 +0900

    ヒーローの画像を表示させる

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from .models import Hero
 
 # Create your views here.
 def hero_list(request):
-    heroes = Hero.objects.all().values_list('name_jp', flat=True)
+    heroes = Hero.objects.all().values_list('name_jp', 'image_url')
     context = {
         'heroes': heroes
     }
