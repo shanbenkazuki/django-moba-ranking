e2481bd463ed44cd9183ad6621cec730107842a2
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-04-01 17:31:04 +0900

    スクレイピングをheroku用に調整

--- main/scraper.py
+++ main/scraper.py
@@ -1,10 +1,11 @@
 import time
+import os
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
@@ -15,12 +16,21 @@ def scrape_mlbb_meta_data():
     DISPLAY_URL = "https://m.mobilelegends.com/en/rank"
     WAIT_TIME = 10
 
+    # ローカル環境で実行する場合
     # chrome_options = Options()
     # chrome_options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    # service = Service(ChromeDriverManager().install())
 
-    service = Service(ChromeDriverManager().install())
+    # Heroku環境で実行する場合
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-    driver = webdriver.Chrome(service=service)
+
+    driver = webdriver.Chrome()
     driver.implicitly_wait(WAIT_TIME)
     driver.get(DISPLAY_URL)
 
