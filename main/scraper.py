b46fb5227ba8b3918d06257db7f8fccabbe30809
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-04-01 18:42:30 +0900

    heroku-buildpack-chrome-for-testing を使用したのでデフォルトでOK

--- main/scraper.py
+++ main/scraper.py
@@ -26,11 +26,9 @@ def scrape_mlbb_meta_data():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
-    driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    driver = webdriver.Chrome()
+    service = Service()  # デフォルトのChromeDriverパスを使用
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.implicitly_wait(WAIT_TIME)
     driver.get(DISPLAY_URL)
 
