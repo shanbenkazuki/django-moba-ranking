da899769e0d6c7082e6a3fcab62b5ea673fbdcc5
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-04-01 22:56:06 +0900

    fdsa

--- main/scraper.py
+++ main/scraper.py
@@ -34,8 +34,8 @@ def scrape_mlbb_meta_data():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
 
-    # service = Service()  # Heroku環境で実行する場合
-    service = Service(ChromeDriverManager().install())  # ローカル環境で実行する場合
+    service = Service()  # Heroku環境で実行する場合
+    # service = Service(ChromeDriverManager().install())  # ローカル環境で実行する場合
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.implicitly_wait(WAIT_TIME)
     driver.get(DISPLAY_URL)
